fd6821033ea8d912a6faf6f831480902df0a5044
pawel <jakowczyk87@gmail.com>
2021-04-06 21:23:32 +0200

    tests refactored

--- tests/RoutingMiddlewareTest.php
+++ tests/RoutingMiddlewareTest.php
@@ -4,14 +4,18 @@ declare(strict_types=1);
 
 namespace pj\routing\tests;
 
+use Closure;
 use Laminas\Diactoros\ServerRequestFactory;
+use Laminas\Diactoros\Response;
 use PHPUnit\Framework\TestCase;
 use pj\routing\RoutingMiddleware;
 use Symfony\Component\Routing\Exception\NoConfigurationException;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+
 
 class RoutingMiddlewareTest extends TestCase
 {
@@ -30,15 +34,17 @@ class RoutingMiddlewareTest extends TestCase
 
     /**
      * @test
+     * @dataProvider routeExpectations
      */
-    public function itMatchesWithTheRequestToFindTheRightRoute()
+    public function itMatchesWithTheRequestToFindTheRightRoute(string $routeName, Route $route, array $middlewares)
     {
-        $routeCollection = new RouteCollection();
-        $routeCollection->add('home', new Route('/'));
-        $middleware = new RoutingMiddleware($routeCollection);
+        $middleware = new RoutingMiddleware($this->createRouteCollection());
         $middleware->process(
             ServerRequestFactory::fromGlobals(),
-            $this->createMock(RequestHandlerInterface::class)
+            $this->createRequestHandler(
+                fn (ServerRequestInterface $request) =>
+                $this->assertEquals($middlewares, $request->getAttributes()['middlewares'])
+            )
         );
     }
 
@@ -56,5 +62,39 @@ class RoutingMiddlewareTest extends TestCase
     public function itPassesTheMiddlewaresFromRoutingUnderMiddlewaresAttribute()
     {
 
+    }
+
+    private function createRequestHandler(Closure $checkExpectations): RequestHandlerInterface
+    {
+        return new class($checkExpectations) implements RequestHandlerInterface
+        {
+            private Closure $checkExpectations;
+
+            public function __construct(Closure $checkExpectations)
+            {
+                $this->checkExpectations = $checkExpectations;
+            }
+
+            public function handle(ServerRequestInterface $request): ResponseInterface
+            {
+                $checkExpectations = $this->checkExpectations;
+                $checkExpectations($request);
+                return new Response();
+            }
+        };
+    }
+
+    public function routeExpectations(): array
+    {
+        return [
+            ['home', new Route('/'), []]
+        ];
+    }
+
+    private function createRouteCollection(): RouteCollection
+    {
+        $routeCollection = new RouteCollection();
+        $routeCollection->add('home', new Route('/'));
+        return $routeCollection;
     }
 }
